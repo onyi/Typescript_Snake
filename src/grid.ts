f98f9e7494fdf381f6abd924497a6163bf4625bf
Onyi Cho <onyicho@gmail.com>
2019-09-14 12:57:48 -0700

    Add grid to display area

---
+++ src/grid.ts
@@ -0,0 +1,40 @@
+import { SnakeBody } from "./snakebody";
+
+export class Grid {
+    private ctx: CanvasRenderingContext2D;
+
+    private offsetX: number = 0;
+    private offsetY: number = 0;
+    private width: number = 50;
+    private offsetWidth: number = 45;
+    public gridWidth = 700;
+    public gridHeight = 700;
+    public gridOffsetWidth = 700;
+    public gridOffsetHeight = 700;
+    private matrix: Array<SnakeBody> = [];
+    
+    constructor(ctx: CanvasRenderingContext2D, offsetX: number, offsetY: number){
+        this.ctx = ctx;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.gridOffsetWidth = offsetX + this.gridWidth;
+        this.gridOffsetHeight = offsetY + this.gridHeight;
+        console.log(`offset width: ${this.gridOffsetWidth}`);
+        console.log(`offset height: ${this.gridOffsetHeight}`);
+
+        this.matrix = [];
+    }
+
+    draw(){
+
+        for(let i = 0; i < this.gridWidth / this.width ; i++){
+            for(let j = 0; j < this.gridHeight / this.width; j++){
+                this.ctx.fillStyle = 'orange';
+                this.ctx.fillRect(this.offsetX + this.width * i, this.offsetY + this.width * j, this.offsetWidth, this.offsetWidth);
+            }
+        }
+    }
+
+
+
+}
