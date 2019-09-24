06a8e7a22c127a3e7dccbd443c7c54fc2142b8b1
Onyi Cho <onyicho@gmail.com>
2019-09-24 10:53:54 -0700

    add logic to init game

--- index.ts
+++ index.ts
@@ -1,9 +1,28 @@
 import { Game } from "./src/game";
+import './index.scss';
+
 document.addEventListener("DOMContentLoaded", () => {
   const canvas: HTMLCanvasElement = document.querySelector("canvas");
   const game = new Game(canvas);
+
+  const restartButton = document.createElement("button");
+  restartButton.innerHTML = "Restart"
+
+  restartButton.addEventListener("click", () => {
+    game.reset();
+  });
+
+  const root: HTMLElement = document.getElementById("root");
+  root.focus;
+
+  root.appendChild(restartButton);
+
+  document.addEventListener( "keydown", (e: KeyboardEvent) => { 
+    console.log(`Keyboard event`);
+    game.changeSnakeDirection(e); 
+  });
   game.startLoop();
-  setTimeout(() => {
-    game.endLoop();
-  }, 2000);
+  // setTimeout(() => {
+  //   game.endLoop();
+  // }, 2000);
 });
