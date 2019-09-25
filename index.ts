d142dc9370126a2281187f6e8d74e3388fdf8cd2
Onyi Cho <onyicho@gmail.com>
2019-09-24 23:13:30 -0700

    Add entry point typescript file for the main logic

--- index.ts
+++ index.ts
@@ -2,23 +2,42 @@ import { Game } from "./src/game";
 import './index.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas: HTMLCanvasElement = document.querySelector("canvas");
-  const game = new Game(canvas);
 
-  const restartButton = document.createElement("button");
-  restartButton.innerHTML = "Restart"
+  //Generating Frontend components
+  const gameComponents = document.getElementById("gameComponents");
 
-  restartButton.addEventListener("click", () => {
-    game.reset();
-  });
+  const gameButton = document.createElement("button");
+  const resetButton = document.createElement("button");
+  resetButton.innerHTML = "Reset";
+  resetButton.classList.add("gameButton");
+  // gameButton.innerHTML = "Restart"
+  gameButton.classList.add("gameButton");
+  gameButton.setAttribute("id", "gameButton");
 
   const root: HTMLElement = document.getElementById("root");
   root.focus;
 
-  root.appendChild(restartButton);
+  gameComponents.appendChild(gameButton);
+  gameComponents.appendChild(resetButton);
+  gameComponents.classList.add("gameComponents");
+
+  root.appendChild(gameComponents);
+
+
+  // Init game class
+  const canvas: HTMLCanvasElement = document.querySelector("canvas");
+  const game = new Game(canvas);
+
+  gameButton.addEventListener("click", () => {
+    game.startGame();
+  });
+
+  resetButton.addEventListener("click", () => {
+    game.reset();
+  })
 
   document.addEventListener( "keydown", (e: KeyboardEvent) => { 
-    console.log(`Keyboard event`);
+    // console.log(`Key Code: ${e.keyCode}`);
     game.changeSnakeDirection(e); 
   });
   game.startLoop();
