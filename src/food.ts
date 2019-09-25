94657e19dbfb9d7707cfcd00587f0f8c934f2e80
Onyi Cho <onyicho@gmail.com>
2019-09-24 23:12:34 -0700

    Add food as an object

---
+++ src/food.ts
@@ -0,0 +1,67 @@
+export class Food {
+
+    private ctx: CanvasRenderingContext2D;
+
+    private x: number;
+    private y: number;
+    private offsetX: number = 0;
+    private offsetY: number = 0;
+
+    private width: number = 45;
+    private offsetWidth: number = 50;
+
+    constructor(ctx: CanvasRenderingContext2D, offsetX: number, offsetY: number){
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.ctx = ctx;
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    getRandomInt(max: number) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    private updateLocation(width: number, height: number){
+        let maxX = (width - this.offsetX) / this.offsetWidth;
+        let maxY = (height - this.offsetY) / this.offsetWidth;
+
+        // console.log(`Max X: ${maxX}; Max Y: ${maxY}`);
+
+        this.x = this.getRandomInt(maxX) * this.offsetWidth + this.offsetX;
+        this.y = this.getRandomInt(maxY) * this.offsetWidth + this.offsetY;
+
+        // console.log(`X & Y: ${this.x}, ${this.y}`);
+        return [this.x, this.y];
+    }
+
+    update(width: number, height: number){
+
+        return this.updateLocation(width, height);
+        
+    }
+
+    draw(){
+        this.ctx.fillStyle = 'green';
+        this.ctx.fillRect(this.getX(), this.getY(), this.width, this.width);
+
+    }
+
+    isCollided(x: number, y: number){
+        // this.shapes.slice(1).some( snakebody => console.log(`X: ${snakebody.getX()}; Y: ${snakebody.getY()}`) );     
+        return x === this.x && y === this.y;
+    }
+
+
+}
+
+
+// let food = new Food(null, 100, 100);
+
+// food.updateLocation(600, 600);
