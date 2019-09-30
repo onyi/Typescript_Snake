af66a941ab9e6376c63287837a56b6a4657622fc
Onyi Cho <onyicho@gmail.com>
2019-09-30 12:06:13 -0700

    Add config enum for global configuration

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export enum Config {
+
+    WIDTH = 25,
+    CANVAS_WIDTH = 500,
+    CANVAS_HEIGHT = 500,
+    CANVAS_OFFSET_X = 0,
+    CANVAS_OFFSET_Y = 0,
+    INIT_SNAKE_SIZE = 7,
+    TICKRATE = 10,
+
+
+
+}
